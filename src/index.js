4ef6c02936c97c0ab31aa9bcecd05613a9e09ab1
allgoodworks <allgoodworks79@gmail.com>
2020-03-11 10:42:11 +1000

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/css/index.css'
 import registerServiceWorker from './registerServiceWorker';
-
+// 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
